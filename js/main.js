9ed24f6661ef47001152e490c832f9a3ad63c787
Brad Broge <brbroge@gmail.com>
2017-06-20 22:07:07 -0400

    setup event listener for tracks

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 const apiKey = "41f6b5a26693fd92184ddd76aaeef8ef"
 
-// Vue.component('artist', {
-//   props: ['artwork', 'name'],
-//   template: '<div class="artist"><img src="{{artwork}}"><h6>{{name}}</h6></div>'
-// })
-
 // Here comes the Vue
 var songapp = new Vue({
   el: '.body',
@@ -14,7 +9,8 @@ var songapp = new Vue({
   },
   methods: {
     populateArtist: function() {
-      console.log(this.artistName);
+      // console.log(this.artistName);
+      this.artists = [];
       let request = `https://api.soundcloud.com/users/?client_id=${apiKey}&q=${this.artistName}`;
       console.log('api request: ', request);
       fetch(request).then(function(resp) {
@@ -22,21 +18,37 @@ var songapp = new Vue({
           console.log('Error encountered fetching from soundcloud. ', res.status);
           return;
         }
-        console.log(resp.status);
+        // console.log(resp.status);
         // console.log(resp);
         resp.json().then(function(data) {
-          // keep working asshole
           for (let i = 0; i < 4; i++) {
             let newObj = {
               artwork: data[i].avatar_url,
               name: data[i].username
             };
-            console.log(newObj);
+            // console.log(newObj);
             songapp.artists.push(newObj);
           }
         });
       });
       this.artistName = "";
+    },
+    populateTracks: function(e) {
+      // console.log(e);
+      let artist = e.target.parentElement.className.split(' ')[1];
+      console.log(artist);
+      let request = `https://api.soundcloud.com/tracks/?client_id=${apiKey}&q=${artist}`;
+      fetch(request).then(function(resp) {
+        if (resp.status !== 200) {
+          console.log('Error getting tracks', resp.status);
+          return;
+        }
+        // console.log(resp);
+        resp.json().then(function(data) {
+          console.log(data);
+          console.log('do something here');
+        });
+      })
     }
   }
 });
